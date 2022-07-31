5ac01a3666b7f724f3b1eef5f7a9108c18ed4e63
tommy <tmtmtoa@gmail.com>
2022-07-31 11:58:28 +0900

    モジュール化

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,12 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-
   int questionNumber = 0;
 
   @override
@@ -47,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.questionsBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,6 +69,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer =
+                    quizBrain.questionsBank[questionNumber].questionAnswer;
+
+                if (correctAnswer == true) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(() {
                   questionNumber++;
                 });
@@ -96,6 +101,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer =
+                    quizBrain.questionsBank[questionNumber].questionAnswer;
+
+                if (correctAnswer == false) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong');
+                }
                 setState(() {
                   questionNumber++;
                 });
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,58 @@
+import 'package:quizzler/question.dart';
+
+class QuizBrain {
+  List<Question> questionsBank = [
+    Question(
+      q: '2000円札は、既に製造が停止されているでしょうか。マルかバツか、どちらでしょう？',
+      a: true,
+    ),
+    Question(
+      q: '日本の中で、面積が一番小さい都道府県は、大阪府である。',
+      a: false,
+    ),
+    Question(
+      q: 'パンダのしっぽの色は白色でしょうか？',
+      a: true,
+    ),
+    Question(
+      q: 'アイスの賞味期限はなく、きちんと保管されたものであれば、昔のものでも食べられるか。',
+      a: true,
+    ),
+    Question(
+      q: '干支は12種類の動物から成り立っている。',
+      a: true,
+    ),
+    Question(
+      q: 'カタツムリがブロック塀などを這っている時、コンクリートを食べている。',
+      a: true,
+    ),
+    Question(
+      q: '宇宙船に乗ったことがある犬がいる。',
+      a: true,
+    ),
+    Question(
+      q: 'プテラノドンは恐竜である。',
+      a: false,
+    ),
+    Question(
+      q: '中国語で手紙はティッシュペーパーと呼びます。',
+      a: false,
+    ),
+    Question(
+      q: '人気ゲーム「ドラゴンクエスト」のナンバリングタイトル全てに登場しているモンスターは、スライムのみである。',
+      a: false,
+    ),
+    Question(
+      q: '全ての動物の中で、一番妊娠期間が長い動物は、ゾウである。',
+      a: true,
+    ),
+    Question(
+      q: 'タラバガニはカニではない。',
+      a: true,
+    ),
+    Question(
+      q: '「土竜」と書いて、「もぐら」と読む。',
+      a: true,
+    ),
+  ];
+}
